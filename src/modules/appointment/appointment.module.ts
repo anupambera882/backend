af324cc241d82b2ae3df11b1f67f3a04ddafec02
Anupam Bera <anupambera882@gmail.com>
2024-05-22 22:40:45 +0530

    modify

--- src/modules/appointment/appointment.module.ts
+++ src/modules/appointment/appointment.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppointmentService } from './appointment.service';
 import { AppointmentController } from './appointment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../auth/entities/user.entity';
+import { Appointment } from './entities/appointment.entity';
+import { Doctor } from './entities/doctor.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Appointment, Doctor])],
   controllers: [AppointmentController],
   providers: [AppointmentService],
 })
